b36d585db98e4a500280767b9c5560875be63f48
navaneethdev <navaneethdev33@gmail.com>
2025-03-25 15:40:40 +0530

    added removefromcart and import to checkout.js

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,5 +1,5 @@
 
-import {cart} from '../data/cart.js';
+import {cart,removeFromCart} from '../data/cart.js';
 import {products} from '../data/products.js';
 import { formatcurrency } from './utils/money.js';
  
@@ -40,7 +40,7 @@ matchingProduct = product;
             <span class="update-quantity-link link-primary">
             Update
             </span>
-            <span class="delete-quantity-link link-primary">
+            <span class="delete-quantity-link link-primary js-delete-link" data-product-id=${matchingProduct.id}>
             Delete
             </span>
         </div>
@@ -95,6 +95,14 @@ matchingProduct = product;
  })
 
  document.querySelector('.js-order-summary').innerHTML=cartSummaryHTML;
+
+ document.querySelectorAll('.js-delete-link').forEach((link)=>{
+  link.addEventListener('click',()=>{
+   const productId= link.getAttribute('data-product-id')
+   removeFromCart(productId)
+    console.log(cart);
+  })
+ })
 
 
 
