94643823e797f5b2846bf08fdeec07c545f8c715
Navaneeth dev g <navaneethdev33@gmail.com>
2025-07-16 17:02:41 +0530

    Initial commit

---
+++ scripts/data/deliveryOptions.js
@@ -0,0 +1,29 @@
+export const deliveryOptions=[{
+    id:'1',
+    deliveryDays:7,
+    priceCents:0,
+},
+{
+    id:'2',
+    deliveryDays:3,
+    priceCents:499,
+},{
+    id:'3',
+    deliveryDays:1,
+    priceCents:999
+}];
+
+export function getDeliveryOptions(deliveryOptionId){
+       
+    let deliveryOption;
+    deliveryOptions.forEach((option)=>{
+        // console.log(typeof option.id);
+        // console.log(typeof deliveryOptionId);
+        
+           if(option.id===deliveryOptionId){
+
+            deliveryOption=option;
+           }
+    });
+    return deliveryOption 
+}
